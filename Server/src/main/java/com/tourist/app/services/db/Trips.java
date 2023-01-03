8f790ad1f8fb37b1220b0ef567d8aa304db0d586
Ru-kko <verase2003.1@gmail.com>
2023-01-03 16:40:16 -0500

    added tripService

---
+++ Server/src/main/java/com/tourist/app/services/db/Trips.java
@@ -0,0 +1,56 @@
+package com.tourist.app.services.db;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tourist.app.dataBase.BaseRepository;
+import com.tourist.app.dataBase.trips.Trip;
+import com.tourist.app.dataBase.trips.TripsRepository;
+import com.tourist.app.services.DatabaseService;
+
+@Service
+public class Trips extends DatabaseService<Integer, Trip> {
+
+  @Autowired
+  private TripsRepository repo;
+
+  @Override
+  public Trip update(Trip toUpdate) {
+    if (toUpdate.getId() == null)
+      return null;
+
+    Optional<Trip> oldData = repo.getById(toUpdate.getId());
+
+    if (oldData.isEmpty())
+      return null;
+
+    if (toUpdate.getCity() == null)
+      toUpdate.setCity(oldData.get().getCity());
+
+    if (toUpdate.getTourist() == null)
+      toUpdate.setTourist(oldData.get().getTourist());
+
+    if (toUpdate.getStartDate() == null)
+      toUpdate.setStartDate(oldData.get().getStartDate());
+
+    return repo.update(toUpdate);
+  }
+
+  public Long countTouristAtSameDay(Date startDate, Integer cityId) {
+    return repo.countTouristAtSameDay(startDate, cityId);
+  }
+
+  public List<Trip> getTripsFromTourist(Integer touristId) {
+    return repo.getTripsFromTourist(touristId);
+  }
+
+  @Override
+  protected BaseRepository<Integer, Trip> getRepo() {
+    return this.repo;
+  }
+
+}
