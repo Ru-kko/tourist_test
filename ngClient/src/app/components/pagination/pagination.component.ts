96ede307c81ef50d95331d4b7be16b1cc4a1d300
Ru-kko <verase2003.1@gmail.com>
2023-03-02 15:57:21 -0500

    pagination component

---
+++ ngClient/src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css'],
+})
+export class PaginationComponent implements OnInit {
+  @Input() pages: number = 1;
+  @Input() actual: number = 1;
+  @Input() onclick?: (n: number) => void;
+
+  range!: number[];
+    
+  ngOnInit(): void {
+    this.range = this.getPagingRange(this.actual, this.pages);
+    console.log(this.range, this.actual, this.pages);
+    
+  }
+
+  clickDecrease() {
+    if (this.actual > 1) {
+      this.onclick?.(this.actual - 1);
+      this.range = this.getPagingRange(this.actual, this.pages);
+    }
+  }
+  clickIncrease() {
+    if (this.actual < this.pages) {
+      this.onclick?.(this.actual + 1);
+      this.range = this.getPagingRange(this.actual, this.pages);
+    }
+  }
+
+  getPagingRange(n: number, total: number): number[] {
+    let length = 5;
+
+    if (length > total) length = total;
+
+    let start = n - Math.floor(length / 2);
+    start = Math.max(start, 1);
+    start = Math.min(start, 1 + total - length);
+
+    return Array.from({ length: length }, (_, i) => start + i);
+  }
+}
