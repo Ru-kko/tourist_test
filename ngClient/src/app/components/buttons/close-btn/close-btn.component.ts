b9c861e85b9dfd6dc58fc01bb9a49f88a108b55e
Ru-kko <verase2003.1@gmail.com>
2023-03-02 14:55:42 -0500

    colse button component

---
+++ ngClient/src/app/components/buttons/close-btn/close-btn.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'open-toggle-btn',
+  templateUrl: './close-btn.component.html',
+  styleUrls: ['./close-btn.component.css'],
+})
+export class CloseBtnComponent {
+  @Input() size: String = '50px';
+  @Input() onclick?: (e: MouseEvent, status: btnState) => void;
+  @Input() initialState: btnState = 'off';
+
+  className: btnState = this.initialState;
+
+  handleClick(e: MouseEvent) {
+    switch (this.className) {
+      case 'off':
+        this.className = 'on';
+        break;
+      case 'on':
+        this.className = 'off';
+        break;
+    }
+    this.onclick?.(e, this.className);
+  }
+}
+
+type btnState = 'on' | 'off';
