295df52ed3791388e10ada18bfe4849904d32540
Ru-kko <verase2003.1@gmail.com>
2023-03-02 14:35:36 -0500

    added page transition and responsive design

--- ngClient/src/app/components/banner/banner.component.ts
+++ ngClient/src/app/components/banner/banner.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { DelayedLinkComponent } from "../buttons/delayed-link/delayed-link.component";
+import { DelayedLinkComponent } from '../buttons/delayed-link/delayed-link.component';
 
 @Component({
-    standalone: true,
-    selector: 'home-Banner',
-    templateUrl: './banner.component.html',
-    styleUrls: ['./banner.component.css'],
-    imports: [DelayedLinkComponent]
+  standalone: true,
+  selector: 'home-Banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css'],
+  imports: [DelayedLinkComponent],
 })
 export class BannerComponent {
+  class: String = "";
+  onClick() {
+    this.class = "out"
+    console.log(this.class);
+  }
 }
