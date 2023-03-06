144b8d8c82497790b112363e9bfcebce4c371dd3
Ru-kko <verase2003.1@gmail.com>
2023-03-06 01:59:05 -0500

    fixed error with common module

--- ngClient/src/app/app.module.ts
+++ ngClient/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,7 +10,6 @@ import { NavigationModule } from './modules';
   ],
   imports: [
     BrowserModule,
-    CommonModule,
     AppRoutingModule,
     NavigationModule
   ],
