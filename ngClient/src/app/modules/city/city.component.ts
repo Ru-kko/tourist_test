365949c00caef1eed8282e0b3c62a443de9745a1
Ru-kko <verase2003.1@gmail.com>
2023-03-06 02:52:57 -0500

    sample view of city page

--- ngClient/src/app/modules/city/city.component.ts
+++ ngClient/src/app/modules/city/city.component.ts
@@ -1,12 +1,81 @@
 import { Component } from '@angular/core';
+import { Headers } from '../navigation.routes/components';
 
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
-  styleUrls: ['./city.component.css']
+  styleUrls: ['./city.component.css'],
 })
 export class CityComponent {
-  constructor() {
-    console.log("fooo"); 
-  }
+  testData: City[] = [
+    {
+      id: 10,
+      name: 'dsfds',
+      population: 100000,
+      mostTuristicPlace: 'foo',
+      mostReserverdHotel: 'bar',
+    },
+    {
+      id: 10,
+      name: 'fdsfds',
+      population: 100000,
+      mostTuristicPlace: 'gfd',
+      mostReserverdHotel: 'bar',
+    },
+    {
+      id: 18,
+      name: 'sdas',
+      population: 100000,
+      mostTuristicPlace: 'fgd',
+      mostReserverdHotel: 'fgdg',
+    },
+    {
+      id: 11,
+      name: 'dfsfs',
+      population: 100000,
+      mostTuristicPlace: 'ddfg',
+      mostReserverdHotel: 'fgd',
+    },
+    {
+      id: 23,
+      population: 100000,
+      name: 'wew',
+      mostTuristicPlace: 'fodsao',
+      mostReserverdHotel: 'bdsaar',
+    },
+    {
+      id: 54,
+      name: 'fsdfsd',
+      population: 100000,
+      mostTuristicPlace: 'hgfo',
+      mostReserverdHotel: 'asr',
+    },
+  ];
+  headers: Headers<City> = {
+    id: {
+      name: 'id',
+      render: false,
+    },
+    name: {
+      name: 'Name',
+      preprocess: (s) => (<string>s).toUpperCase(),
+    },
+    population: {
+      name: 'Population',
+    },
+    mostTuristicPlace: {
+      name: 'Most Turistic Place',
+    },
+    mostReserverdHotel: {
+      name: 'Most Reserverd Hotel',
+    },
+  };
+}
+
+export interface City {
+  id: number;
+  name: string;
+  population: number;
+  mostTuristicPlace: string;
+  mostReserverdHotel: string;
 }
--- ngClient/src/app/modules/city/city.module.ts
+++ ngClient/src/app/modules/city/city.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
+import { NavigationModule } from '../';
 import { CityRoutingModule } from './city-routing.module';
 import { CityComponent } from './city.component';
 
-
-
 @NgModule({
-  declarations: [
-    CityComponent
-  ],
-  imports: [
-    CityRoutingModule
-  ]
+  declarations: [CityComponent],
+  imports: [CityRoutingModule, NavigationModule],
 })
 export class CityModule {}
